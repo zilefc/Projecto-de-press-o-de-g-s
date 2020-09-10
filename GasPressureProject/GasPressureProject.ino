aaa9f37e13ef10220fcb111b4190045e184ea1cd
zilefc <fczile@gmail.com>
2020-09-10 10:08:40 +0200

    Update GasPressureProject.ino

--- GasPressureProject/GasPressureProject.ino
+++ GasPressureProject/GasPressureProject.ino
@@ -7,83 +7,89 @@
 #include <String.h>
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#include <LiquidCrystal_I2C.h>
 /*
-Vin 3.3V
-GND GND
-SCL GPIO 5 (D1)
-SDA GPIO 4 (D2)
+SCL A5
+SDA A4
  */
+SoftwareSerial sim(12, 13);//D7-13  D8=15 TX RX 
+LiquidCrystal_I2C display(0x3F,16,2); 
 
-SoftwareSerial sim(13, 15);//D7  D8 TX RX 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+String numero = "+258848856808";//
 
-String numero = "+258878780367";
-
-const float SensorOffset = 189;
+const float SensorOffset = 50;
 float Pressure;     
-float Resistor = 162;  //reference resistor resistance
-float Pmax = 500;           //Max of process variable range
-float Pmin = 0;             //Min of process variable range
-float Vref = 5;             //Reference voltage for AnalogRead
+float Resistor = 154.09;  //154.09
+float Pmax = 500;
+float Pmin = 0;
+float Vref = 3.3;
 
 
 void setup() {
-  sim.begin(115200);
-  delay(1000);     
-  Serial.begin(115200); 
+   Serial.begin(9600);
+   sim.begin(9600);
+   display.init();
+   display.backlight();
+   display.setCursor(3,0);
+   display.print("Bem-Vindo!");
+   display.setCursor(0,1);
+   display.print("Sistema iniciado");
+   delay(1000);     
+  
   Serial.println("Time, Raw reading, Corrected reading");
-
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  
-  display.clearDisplay();
-  display.drawFastHLine(0, 32, SSD1306_LCDWIDTH, WHITE);
-  display.setTextSize(1);
-  display.setTextColor(WHITE, BLACK);
-  display.setCursor(10, 5);
-  display.print("sensorValue(PSI)");
-  display.setCursor(19, 37);
-  display.print("Percentagem (%)");
-  display.display();
+  comeco();
 }
-
+void comeco()
+{
+  sim.println("AT+CMGF=1");
+  delay(1000);
+  sim.println("AT+CMGS=\"" + numero + "\"\r");
+  delay(1000);
+  sim.println("Sistema iniciado. Veja os dados da leitura em tempo real em: https://thingspeak.com/channels/1134042/widgets/213604 ");
+  delay(100);
+  sim.println((char)26);
+  delay(1000);
+  }
 void loop() {
   
   Serial.print(millis());
   Serial.print(",");
   //Read the voltage across the reference resistor
   Pressure = analogRead(A0);
+  Pressure =Pressure +SensorOffset;
   int Percentage=map(Pressure,192,1023,0,100);
   //Pressure=Pressure-SensorOffset;
   Serial.print(Pressure);
   Serial.print(",");
-  //Convert the voltage signal to a pressure reading
   Pressure =(Pmax-Pmin)*Vref/(1023*Resistor*(0.02-0.004))*(Pressure-1023*0.004*Resistor/Vref);
+  Pressure=Pressure+14;
   Serial.println(Pressure);
   delay(200);
   
-  display.setCursor(40, 20);
+  display.clear();
+  display.setCursor(0,0);
+  display.print("Pressure: ");
+  display.setCursor(10,0);
   display.print(Pressure);
-  display.setCursor(40, 52);
+  display.setCursor(13,0);
+  display.print("PSI");
+  display.setCursor(0, 1);
+  display.print("Percentage: ");
+  display.setCursor(12,1);
   display.print(Percentage);
-  display.display();
-  delay(1000);
-
+  display.setCursor(15,1);
+  display.print("%");
   post();
-  delay(1000);
-  if(Percentage<21){
-    mensagem();
-    delay(100);
-    }
+  esperar();
   
 }
  void post()
  {
   Pressure = analogRead(A0);
+  Pressure =Pressure +SensorOffset;
   int Percentage=map(Pressure,192,1023,0,100);
   Pressure =(Pmax-Pmin)*Vref/(1023*Resistor*(0.02-0.004))*(Pressure-1023*0.004*Resistor/Vref); 
+  Pressure=Pressure+14;
   if (sim.available())
     Serial.write(sim.read());
  
@@ -138,15 +144,14 @@ void ShowSerialData()
   while(sim.available()!=0)
   Serial.write(sim.read());
   delay(5000); 
-  
 }
 void mensagem()
 {
  Pressure = analogRead(A0);
  int Percentage=map(Pressure,192,1023,0,100);
  Pressure =(Pmax-Pmin)*Vref/(1023*Resistor*(0.02-0.004))*(Pressure-1023*0.004*Resistor/Vref);
+ Pressure=Pressure+14;
  delay(100);   
-   
   sim.println("AT+CMGF=1");
   delay(1000);
   sim.println("AT+CMGS=\"" + numero + "\"\r");
@@ -161,3 +166,11 @@ void mensagem()
   sim.println((char)26);
   delay(1000);
 }
+void esperar()
+{
+  for(int x=1;x<40;x++)
+  {
+    delay(30000);
+    Serial.println("Meio segundo");
+    }
+  }
